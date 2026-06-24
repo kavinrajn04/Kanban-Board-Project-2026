c720ee36c0087a3e35dc15160e2ea5c69fb9dbae
kavinrajn04 <kavinrajn0402@gmail.com>
2026-06-24 17:29:55 +0530

    kavinrajn

--- myapp/models.py
+++ myapp/models.py
@@ -1,17 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
 
-class userData(models.Model):
-    PRIORITY_CHOICES = [
-        ("low", "Low"),
-        ("medium", "Medium"),
-        ("high", "High"),
-    ]
-    title = models.CharField(max_length=20)
-    priority = models.CharField(
-        max_length=10,
-        choices=PRIORITY_CHOICES,
-        default="medium",
-    )
+class Note(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    text = models.TextField()
--- myapp/urls.py
+++ myapp/urls.py
@@ -4,6 +4,5 @@ from . import views
 
 urlpatterns = [
     path("", views.login_view, name="login"),
-    path("sidebar/", views.sidebar_view, name="sidebar"),
-    path("dashboard/", views.dashboard_view, name="dashboad"),
+    path("notes/", views.notes_view, name="notes"),
 ]
